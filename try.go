c32e6496660d72ef6b92c86c74eb5452e892a356
Jackson Sabey <jackson@juandefu.ca>
2016-07-31 02:39:59 -0700

    renamed namespace moved namespace above copyright

--- try.go
+++ try.go
@@ -1,6 +1,7 @@
+package try
+
 // Copyright 2015, JuanDeFu.ca. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
-package try
 
 func Try(
 	max int,
